d866db9fc1561f74810e1f473942d7f99c64bd79
Paul <bellevillepaul03@gmail.com>
2025-05-24 21:04:20 +0200

    article extractor done, will add more newspaper to be supported later

--- S2/Exam/article_extractor.py
+++ S2/Exam/article_extractor.py
@@ -1,26 +1,27 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait, Select
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException, StaleElementReferenceException, TimeoutException
 import time
-import os
 
 
-def scrap_article(url:str):
+def scrap_article(url:str) -> list:
+    """
+    Fonction qui scrappe le contenu de l'article donné en URL et renvoie les données.
+    Fonctionne avec LeParisien, RFI et LeMonde.
+    """
+    # Initialisation de Selenium
     service = Service(executable_path=r"C:\Program Files (x86)\geckodriver.exe")
     options = webdriver.FirefoxOptions()
-    #options.add_argument("--headless")
+    # Mode headless pour économie de ressources et juste plus de confort
+    options.add_argument("--headless")
     options.add_argument("--disable-cookies")
     options.binary_location = r"C:\Program Files\Mozilla Firefox\firefox.exe"
-
     driver = webdriver.Firefox(service=service, options=options)
-    
+    # Accès à l'URL désirée
     driver.get(url)
     xpath = ""
+    # Selon les sites, l'accès au bouton pour fermer le pop-up concernant les cookies va être différent
+    # Ici on regarde quel site on doit scrapper pour trouver le bon Xpath afin de cliquer sur le bon bouton
     if "lemonde.fr" in url:
         xpath = "/html/body/div[10]/div/footer/button"
     elif "rfi.fr" in url:
@@ -31,18 +32,27 @@ def scrap_article(url:str):
     accepter = driver.find_element(By.XPATH, xpath)
     accepter.click()
 
+    article_text = []
     # Supposition selon laquelle le titre est forcément un <h1> avec 'title' dans le nom de la classe
     title = driver.find_element(By.XPATH, "//h1[contains(@class, 'title')]")
-    print(title.text)
+    article_text.append(title.text.strip())
     # Logique selon laquelle tout le text de l'article est dans une balise <p>
     text = driver.find_elements(By.TAG_NAME, "p")
-    for i in text:
-        print(i.text)
+    for t in text:
+        article_text.append(t.text.strip())
     # Ces deux suppositions ont été vérifiées pour les sites d'articles supporté par le scrapper.
 
+    # Fermeture du Firefox
+    # En principe ce n'est pas nécessaire de le faire sur une machine personnelle mais si le script venait à être utilisé sur un serveur via un scronjob, cela pourrait très vite causer des problèmes. 
     driver.close()
 
-scrap_article("https://www.rfi.fr/fr/europe/20250524-crise-politique-en-serbie-des-universitaires-lancent-une-p%C3%A9tition-pour-des-l%C3%A9gislatives-anticip%C3%A9es")
-#scrap_article("https://www.leparisien.fr/loiret-45/cette-ville-du-loiret-met-en-vente-une-maisona-0-euro-25-04-2025-KLRQ5QUNUJHUXGY5BADFPZGBWY.php")
-#scrap_article("https://www.rfi.fr/en/international/20250524-macron-heads-to-vietnam-as-france-seeks-bigger-role-in-indo-pacific")
-#scrap_article("https://www.lemonde.fr/international/article/2025/04/15/manifestations-en-serbie-les-etudiants-bloquent-la-radio-television-publique_6596235_3210.html")
+    return " ".join(article_text)
+
+if __name__ == "__main__":
+    # Articles sample
+
+    article = scrap_article("https://www.rfi.fr/fr/europe/20250524-crise-politique-en-serbie-des-universitaires-lancent-une-p%C3%A9tition-pour-des-l%C3%A9gislatives-anticip%C3%A9es")
+    print(article)
+    #scrap_article("https://www.leparisien.fr/loiret-45/cette-ville-du-loiret-met-en-vente-une-maisona-0-euro-25-04-2025-KLRQ5QUNUJHUXGY5BADFPZGBWY.php")
+    #scrap_article("https://www.rfi.fr/en/international/20250524-macron-heads-to-vietnam-as-france-seeks-bigger-role-in-indo-pacific")
+    #scrap_article("https://www.lemonde.fr/international/article/2025/04/15/manifestations-en-serbie-les-etudiants-bloquent-la-radio-television-publique_6596235_3210.html")
